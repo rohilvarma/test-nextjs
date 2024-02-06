8e5d0cbb4cf3f98766764a39796b1ba86c2b49eb
rohilvarma <rohilvarma96@gmail.com>
2024-02-06 15:26:24 +0530

    updated URL

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Suspense } from "react";
 
 async function testGet() {
-  const res = await fetch("http:localhost:3000/api", {
+  const res = await fetch(`${process.env.URL}/api`, {
     method: "GET",
     cache: "no-store",
   });
